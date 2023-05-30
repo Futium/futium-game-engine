3a63a4631f644d8670596ce42edbd18705f32f8a
muriatic <64611874+muriatic@users.noreply.github.com>
2023-05-30 13:46:07 -0400

    create triangle on screen

--- main.cpp
+++ main.cpp
@@ -2,6 +2,23 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+
+// shader code
+// shaders are openGL object
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\n";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.92f, 0.25f, 0.20f, 1.0f);\n"
+"}\n\0";
+
 int main() {
 	
 	// initiliaze glfw
@@ -14,6 +31,20 @@ int main() {
 
 	// only modern GLFW functions (CORE profile)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// array of datatype GLfloat
+	// every 3 floats will represent one coord
+
+	// equilateral triangle
+	GLfloat vertices[] =
+	{
+		// left corner
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		// right corner
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		// top corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+	};
 
 	// create window				(width, height, "Name of Window", fullScreenOrNot, NULL
 	GLFWwindow* window = glfwCreateWindow(800, 800, "Futium Game Engine", NULL, NULL);
@@ -34,26 +65,119 @@ int main() {
 	// tell it that we want the viewport to be, (x0, y0 to x1, y1)
 	glViewport(0, 0, 800, 800);
 
-	/*
-	REMEMBER, YOU HAVE TWO DISPLAYS, THE CURRENT VIEW AND THE BUFFER WHICH IS WHAT WILL BE RENDERED NEXT
-	
-	FRONT BUFFER AND BACK BUFFER
-	*/
+	// create reference aka value to store vertex shader in
 
-	// tell opengl to prepare to change the screen to next page
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	// openGL version of unsigned integer
+	//						specify we want vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
-	// execute command on color buffer... clean back buffer
-	glClear(GL_COLOR_BUFFER_BIT);
+	//			  ref value,    1 screen, source code,   NULL
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+
+	// have to compile it for the GPU to understand this 
+	glCompileShader(vertexShader);
+
+	// same as above but fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// have to wrap in shader program
+	GLuint shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	// wrap up GL shader program
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// when you send info from CPU to GPU, you want to send it in big batches, called buffers 
+	// not like front and end buffers below
+	// so we create vertexBuffer obj
+
+	GLuint VAO, VBO;
+
+	// ORDER MATTERS, MUST BE BEFORE VBO
+	// VAOs allow you to quickly switch between different VBOs
+	glGenVertexArrays(1, &VAO);
+
+	// binding makes a certain object the current obj
+	// if you make a function that modifies the certain object it modifies the current obj
+	glGenBuffers(1, &VBO);
+
+	// bind VAO so we can work with it
+	glBindVertexArray(VAO);
+
+	// see other types of buffers
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	// store vertices
+	// fourth arg can be (improves perf):
+	// stream - vertices will be modified once and used a few times
+	// static - vertices will be modified once and used many many times
+	// dynamic - vertices will be modified multiple times and used many many times
+	// also choose between:
+	// draw - vertices will be modified and used to draw an image on the screen
+	// read - vertices will be read
+	// copy - vertices will be copied
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	// configure VAO
+	// 1. Position index of vertex attrib which allows us to communicate with the vertex shader from the outside
+	// 2. number of values per vertex 
+	// 3. datatype 
+	// 4. only matters if coords are ints else GL_FALSE
+	// 5. stride of our vertices - amt of data between each vertex so with 3 floats, then 3 * sizeof one float
+	// 6. offset - pointer to where our vertices begin in the array, since its at the beginning we just do (void*)0
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	// enable it. 0 is position of vertex attribute
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
 
 	// swap the front buffer with back buffer
 	glfwSwapBuffers(window);
 
 	// dont close until given close input
 	while (!glfwWindowShouldClose(window)) {
+		/*
+		REMEMBER, YOU HAVE TWO DISPLAYS, THE CURRENT VIEW AND THE BUFFER WHICH IS WHAT WILL BE RENDERED NEXT
+	
+		FRONT BUFFER AND BACK BUFFER
+		*/
+
+		// tell opengl to prepare to change the screen to next page
+		glClearColor(0.93f, 1.0f, 0.99f, 1.0f);
+
+		// execute command on color buffer... clean back buffer
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// activate the shader program
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+
+		// (type of primitive, starting index of vert, amount of vertices)
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// swap the buffers so the image gets updated each frame
+		glfwSwapBuffers(window);
+
 		// tell it to process the poll events, w/o it will stay in not responding forever
 		glfwPollEvents();
 	}
+
+	// delete the objs we created
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
 
 	// destroy the window
 	glfwDestroyWindow(window);
@@ -63,3 +187,4 @@ int main() {
 
 	return 0;
 }
+
